e145166507a8e22b1824a34a7d3160c62b18f94d
Agentkow <kennethkhau212@gmail.com>
2018-04-16 15:31:02 -0500

    added prefabs

---
+++ Assets/Scripts/Spin.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spin : MonoBehaviour 
+{
+    private float force = 100;
+    private Rigidbody rb;
+
+	// Use this for initialization
+	void Start () 
+	{
+        rb = GetComponent<Rigidbody>();
+	}
+
+    void OnTriggerEnter(Collider other)
+    {
+        rb.AddForce(transform.up*force, ForceMode.Acceleration);
+    }
+}
