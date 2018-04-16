45a04012cae235ef9251c02dc700d96d94f8df33
Agentkow <kennethkhau212@gmail.com>
2018-04-16 12:37:16 -0500

    First

---
+++ Assets/Scripts/BreakAway.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BreakAway : MonoBehaviour {
+
+    [SerializeField]
+    private float force = 100f;
+
+    private Rigidbody rb;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        rb.AddForce(-transform.forward * force, ForceMode.Acceleration);
+        rb.useGravity = true;
+    }
+}
---
+++ Assets/Scripts/Kick.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Kick : MonoBehaviour 
+{
+    public Transform playerTransform;
+
+    [SerializeField]
+    private float kickForce = 20;
+
+    [SerializeField]
+    private Transform cameraTransform;
+
+    private Rigidbody rigBody;
+    private string kickName;
+    private bool canKick;
+
+    private void Awake()
+    {
+        rigBody = GetComponent<Rigidbody>();
+        playerTransform = GetComponent<Transform>();
+    }
+
+    private void Start()
+    {
+        kickName = "Fire1";
+    }
+
+    // Update is called once per frame
+    void FixedUpdate () 
+	{
+        if (Input.GetButton(kickName))
+        {
+            KickLaunch();
+        }
+	}
+
+    void KickLaunch()
+    {
+        rigBody.AddForce(kickForce * cameraTransform.forward + transform.up);
+    }
+    
+}
